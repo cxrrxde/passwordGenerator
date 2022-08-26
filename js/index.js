e5b7a11496341e812e487449c2c9e2f57520711f
cxrrxde <111563147+cxrrxde@users.noreply.github.com>
2022-08-26 12:18:14 +0500

    Added some features
    - When you click to copy you will get notification
    - Now you can choose to generate password without symbols or without numbers

--- js/index.js
+++ js/index.js
@@ -14,30 +14,105 @@ let secondField = document.querySelector("#second-field")
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 let passwordLength = slider.value
 let usedCharacters = []
+let symbolsEnabled = document.querySelector("#symbols-toggle").checked
+let numbersEnabled = document.querySelector("#numbers-toggle").checked
 
+function enableCheckbox(){
+    let check1 = document.getElementById("symbols-toggle")
+    if(check1.checked){
+        symbolsEnabled = true
+    }
+    else{
+        symbolsEnabled = false
+    }
+    let check2 = document.getElementById("numbers-toggle")
+    if(check2.checked){
+        numbersEnabled = true
+    }
+    else{
+        numbersEnabled = false
+    }
+}
 
 function generatePass(){
     let firstPassword = ""
-    while (firstPassword.length < passwordLength){
-        firstPassword += characters[generateRandomNumber()]
-    }
-    firstField.value = firstPassword
     let secondPassword = ""
-    while (secondPassword.length < passwordLength){
-        secondPassword += characters[generateRandomNumber()]
+    if (symbolsEnabled === true && numbersEnabled === true){
+        while (firstPassword.length < passwordLength){
+            firstPassword += characters[generateRandomNumber()]
+        }
+        firstField.value = firstPassword
+        while (secondPassword.length < passwordLength){
+            secondPassword += characters[generateRandomNumber()]
+        }
+        secondField.value = secondPassword
     }
-    secondField.value = secondPassword
+    else if (symbolsEnabled === true && numbersEnabled === false){
+        while (firstPassword.length < passwordLength){
+            firstPassword += characters[generateWithOutNumbers()]
+        }
+        firstField.value = firstPassword
+        while (secondPassword.length < passwordLength){
+            secondPassword += characters[generateWithOutNumbers()]
+        }
+        secondField.value = secondPassword
+    }
+    else if (symbolsEnabled === false && numbersEnabled === true){
+        while (firstPassword.length < passwordLength){
+            firstPassword += characters[generateWithOutSymbols()]
+        }
+        firstField.value = firstPassword
+        while (secondPassword.length < passwordLength){
+            secondPassword += characters[generateWithOutSymbols()]
+        }
+        secondField.value = secondPassword
+    }
+    else if (symbolsEnabled === false && numbersEnabled === false){
+        alert("Please, enable symbols or numbers")
+    }
+}
+
+function generateWithOutSymbols(){
+    let withOutSymbols = generateRandomNumber()
+    let indexesOfCharacters = []
+    for (let i = 0; i < characters.length; i++){
+        for (let k = 0; k < 10; k++){
+            if (characters[i] == k){
+                indexesOfCharacters.push(i)
+            }
+        }
+    }
+    while(indexesOfCharacters.includes(withOutSymbols) === false){
+        withOutSymbols = generateRandomNumber()
+    }
+    return withOutSymbols
+}
+
+function generateWithOutNumbers(){
+    let withOutNumber = generateRandomNumber()
+    let indexesOfCharacters = []
+    for (let i = 0; i < characters.length; i++){
+        for (let k = 0; k < 10; k++){
+            if (characters[i] == k){
+                indexesOfCharacters.push(i)
+            }
+        }
+    }
+    while(indexesOfCharacters.includes(withOutNumber) === true){
+        withOutNumber = generateRandomNumber()
+    }
+    return withOutNumber
 }
 
 function generateRandomNumber(){
-    let randomNumber = Math.floor(Math.random()*91)
+    let randomNumber = Math.floor(Math.random()*characters.length)
     if (usedCharacters.length === 0){
         usedCharacters.push(randomNumber)
         return randomNumber
     }else{
             if (usedCharacters[usedCharacters.length-1] === randomNumber){
                 while (usedCharacters[usedCharacters.length-1] === randomNumber){
-                    randomNumber = Math.floor(Math.random()*91)
+                    randomNumber = Math.floor(Math.random()*characters.length)
                 }
                 usedCharacters.push(randomNumber)
             }else{
@@ -49,10 +124,18 @@ function generateRandomNumber(){
 //----------------------------------------------------------WORKING WITH "COPY-ON-CLICK"
 document.getElementById("first-field").addEventListener("click",function(){
         let copiedTextFirst = document.querySelector("#first-field").value
-        navigator.clipboard.writeText(copiedTextFirst)
+        navigator.clipboard.writeText(copiedTextFirst).then(function(){
+            if (copiedTextFirst !== ""){
+                alert("Password:    " + copiedTextFirst + "    copied to clipboard!")
+            }
+        })
+
 })
 document.getElementById("second-field").addEventListener("click",function(){
         let copiedTextSecond = document.querySelector("#second-field").value
-        navigator.clipboard.writeText(copiedTextSecond)
+        navigator.clipboard.writeText(copiedTextSecond).then(function(){
+            if (copiedTextSecond !== ""){
+                alert("Password:    " + copiedTextSecond + "    copied to clipboard!")
+            }
+        })
 })
-
